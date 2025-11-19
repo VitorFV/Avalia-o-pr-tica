287040c56c7f6b8a866e70aa263379c78c84d9ee
VitorFV <Vitor_pieri@estudante.sesisenai.org.br>
2025-11-18 21:58:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+let Tocando = false;
+let audio = new Audio('Unshaken.mp3');
+
+function tocarMusicaRed() {
+    if (!Tocando) {
+        audio.play();
+        Tocando = true;
+    } else {
+        audio.pause();
+        Tocando = false;
+    }
+}
+
+let Tocando1 = false;
+let audio1 = new Audio('Orbital.mp3');
+
+function tocarMusicaBattle6() {
+    if (!Tocando1) {
+        audio1.play();
+        Tocando1 = true;
+    } else {
+        audio1.pause();
+        Tocando1 = false;
+    }
+}
+
+let Tocando2 = false;
+let audio2 = new Audio('TheWorldRevolving.mp3');
+
+function tocarMusicaDelta() {
+    if (!Tocando2) {
+        audio2.play();
+        Tocando2 = true;
+    } else {
+        audio2.pause();
+        Tocando2 = false;
+    }
+}
+
+let Tocando3 = false;
+let audio3 = new Audio('MEGALOVANIA.mp3');
+
+function tocarMusicaUnder() {
+    if (!Tocando3) {
+        audio3.play();
+        Tocando3 = true;
+    } else {
+        audio3.pause();
+        Tocando3 = false;
+    }
+}
+
+let Tocando4 = false;
+let audio4 = new Audio('EnterPharloom.mp3');
+
+function tocarMusicaSilk() {
+    if (!Tocando4) {
+        audio4.play();
+        Tocando4 = true;
+    } else {
+        audio4.pause();
+        Tocando4 = false;
+    }
+}
+
+let Tocando5 = false;
+let audio5 = new Audio('SubwooferLullaby.mp3');
+
+function tocarMusicaMine() {
+    if (!Tocando5) {
+        audio5.play();
+        Tocando5 = true;
+    } else {
+        audio5.pause();
+        Tocando5 = false;
+    }
+}
+
+let Tocando6 = false;
+let audio6 = new Audio('HellOnEarth.mp3');
+
+function tocarMusicaDoom() {
+    if (!Tocando6) {
+        audio6.play();
+        Tocando6 = true;
+    } else {
+        audio6.pause();
+        Tocando6 = false;
+    }
+}
+
+let Tocando7 = false;
+let audio7 = new Audio('FragmentsOfTheMountain.mp3');
+
+function tocarMusicaCeleste() {
+    if (!Tocando7) {
+        audio7.play();
+        Tocando7 = true;
+    } else {
+        audio7.pause();
+        Tocando7 = false;
+    }
+}
